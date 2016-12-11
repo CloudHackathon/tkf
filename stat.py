221851ccad8379d51c0b08c2c7f2ac9d862025f3
Kinka <kinkabrain@gmail.com>
2016-12-11 16:09:36 +0800

    pretty output

--- stat.py
+++ stat.py
@@ -1,3 +1,4 @@
+from pprint import pprint
 ups = ['SH603036', 'SH600239', 'SH603990', 'SH600173', 'SH600909', 'SH601882', 'SH600466', 'SH603819', 'SH601116', 'SH603033']
 downs = ["SH603025", "SH603159", "SH603900", "SH603987", "SH603977", "SH603060", "SH603323", "SH600848", "SH603999", "SH603727"]
 def calc(f):
@@ -15,9 +16,21 @@ def calc(f):
 
 cats = {'user.time': [], 'user.reply': [], 'all.time': [], 'all.alpha': []}
 keys = ['user.time', 'user.reply', 'all.time', 'all.alpha']
+print '\nups\n'
+for f in ups:
+    cat = calc(f)
+    for k in keys:
+        cats[k].append(float('%.2f' % cat[k]))
+
+for k in keys:
+    print k, cats[k]
+
+cats = {'user.time': [], 'user.reply': [], 'all.time': [], 'all.alpha': []}
+print '\ndowns\n'
 for f in downs:
     cat = calc(f)
     for k in keys:
         cats[k].append(float('%.2f' % cat[k]))
 
-print cats
+for k in keys:
+    print k, cats[k]
